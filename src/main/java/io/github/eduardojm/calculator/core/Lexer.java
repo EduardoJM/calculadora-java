8bf945f22db3db92ca24fd99f85230f0ca67132b
Eduardo Oliveira <eduardo_y05@outlook.com>
2021-07-15 17:58:37 -0300

    add javadoc to Lexer

--- src/main/java/io/github/eduardojm/calculator/core/Lexer.java
+++ src/main/java/io/github/eduardojm/calculator/core/Lexer.java
@@ -7,15 +7,33 @@ import io.github.eduardojm.calculator.core.tokens.TokenParentheses;
 
 import java.util.Optional;
 
+/**
+ * A class that implements a Lexer for the calculator.
+ */
 public class Lexer {
+    /**
+     * The input character stream.
+     */
     private final CharStream stream;
+    /**
+     * The current peek token (Can be null).
+     */
     private Token current;
 
+    /**
+     * Creates a new instance of the Lexer.
+     * @param input The input character stream.
+     */
     public Lexer(CharStream input) {
         this.stream = input;
         this.current = null;
     }
 
+    /**
+     * Peek a token. Returns the next token and not skip it position.
+     * @return Returns the next token.
+     * @throws Exception This method can throws a Exception if can't parse the input.
+     */
     public Optional<Token> peek() throws Exception {
         if (this.current != null) {
             return Optional.of(this.current);
@@ -24,6 +42,11 @@ public class Lexer {
         return Optional.ofNullable(this.current);
     }
 
+    /**
+     * Get the next token and skip it position.
+     * @return Returns the next token.
+     * @throws Exception This method can throws a Exception if can't parse the input.
+     */
     public Optional<Token> next() throws Exception {
         Token tok = this.current;
         this.current = null;
@@ -33,16 +56,29 @@ public class Lexer {
         return this.readNext();
     }
 
+    /**
+     * Gets a value indicating if is the end of the stream.
+     * @return Returns a boolean value indicating if is the end of the stream.
+     */
     public boolean eof() {
         return this.stream.eof();
     }
 
+    /**
+     * Parse a number token.
+     * @return Returns the parsed TokenNumber.
+     */
     private TokenNumber parseNumber() {
         String value = this.readWhile(this.isDigit);
         float parsedNumber = Float.parseFloat(value);
         return new TokenNumber(parsedNumber);
     }
 
+    /**
+     * Read the next token.
+     * @return Returns the read next token or Optional.empty() if is end of the stream.
+     * @throws Exception Throws a Exception if the Lexer or Character Stream can't parse the input.
+     */
     private Optional<Token> readNext() throws Exception {
         this.readWhile(this.isWhiteSpace);
         if (this.stream.eof()) {
@@ -61,14 +97,40 @@ public class Lexer {
         throw new Exception("Invalid token: " + ch);
     }
 
+    /**
+     * A CharPredicate that indicate if a character is the white space.
+     */
     private final CharPredicate isWhiteSpace = ch -> ch == ' ';
+    /**
+     * A CharPredicate that indicates if is the start of a number.
+     */
     private final CharPredicate isDigitStart = Character::isDigit;
+    /**
+     * A CharPredicate that indicates if is a part of a digit number.
+     */
     private final CharPredicate isDigit = ch -> Character.isDigit(ch) || ch == '.';
+    /**
+     * A CharPredicate that indicates if the character is a operator.
+     */
     private final CharPredicate isOperator = ch -> ch == '-' || ch == '+' || ch == '*' || ch == '/' || ch == '^';
+    /**
+     * A CharPredicate that indicates if the character is a parentheses.
+     */
     private final CharPredicate isParentheses = ch -> ch == '(' || ch == ')';
+    /**
+     * A CharPredicate that indicates if the character is the start of a identifier.
+     */
     private final CharPredicate isIdentifierStart = ch -> String.valueOf(ch).matches("[A-z_]");
+    /**
+     * A CharPredicate that indicates if the character is a part of a identifier.
+     */
     private final CharPredicate isIdentifier = ch -> isIdentifierStart.test(ch) || isDigitStart.test(ch);
 
+    /**
+     * Read from CharStream while a determinate condition.
+     * @param condition The condition to continue reading.
+     * @return Returns the read string.
+     */
     private String readWhile(CharPredicate condition) {
         StringBuilder str = new StringBuilder();
         while (!this.stream.eof() && condition.test(this.stream.peek())) {
